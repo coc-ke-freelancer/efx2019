985c8fd00e02443435163bfbc740c0906d68550f
billy <billy@pop-os.localdomain>
2019-08-23 20:30:12 +0700

    update schedule

--- schedule.js
+++ schedule.js
@@ -2,6 +2,7 @@ import { crawlOptionBoards } from "./helpers/parseoptionsboards";
 import { crawlInsights } from './helpers/parseinsights';
 import { crawlDataPreview } from './helpers/parsedatapreview';
 import { crawlCookies } from './helpers/fetchcookies';
+const moment = require('moment-timezone');
 import { optionsCookies, optionsBoards, optionsDataPreviews, optionsInsights } from './options'
 
 let NewsModel = require('./models/news');
@@ -54,34 +55,65 @@ let _modules = [
     }
 ]
 
-schedule.scheduleJob("* */12 * * *", async () => {
-    await getCookies();
-});
+// schedule.scheduleJob("* */12 * * *", async () => {
+//     await getCookies();
+// });
 
-for (const iterator of _modules) {
-    schedule.scheduleJob(iterator.schedule, async () => {
-        iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
-            .then(result => {
-                debug(iterator.message);
-            });
-    });
+
+let randomTime = () => {
+    let time = new Date();
+    let listtime = [];
+    let min = 10;
+    let max = 90;
+    let random = [];
+    for (let i = 0; i < 3; i++) {
+        random.push(parseInt(Math.random() * (+max - +min) + +min));
+        listtime.push(moment(time).add(random[i], 'seconds').toString());
+    }
+    return listtime;
 }
+
+
+let scheduleJobs = (listtime) => {
+    debug('Im in schedule', listtime)
+    for (let i = 0; i < listtime.length; i++) {
+        schedule.scheduleJob(listtime[i], async () => {
+            debug('after work: ', listtime[i]);
+        })
+
+        // for (const iterator of _modules) {
+        //     schedule.scheduleJob(listtime[i], async () => {
+        //         iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
+        //             .then(result => {
+        //                 debug('after', listtime[i], ' ', iterator.message);
+        //             });
+        //     });
+        // }
+    }
+
+
+}
+
 
 let boot = async () => {
     debug("Start BOOT !!!");
-    await getCookies();
-    for (const iterator of _modules) {
-        iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
-            .then(result => {
-                debug(iterator.message);
-            });
-    }
+    let listtime = randomTime();
+    scheduleJobs(listtime);
+
+    // await getCookies();
+    // for (const iterator of _modules) {
+    //     iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
+    //         .then(result => {
+    //             debug(iterator.message);
+    //         });
+    // }
 }
 
 boot()
 
-async function getCookies() {
-    await getData(crawlCookies, optionsCookies).then(result => {
-        debug('get Cookies OK !!!');
-    });
-}
+// async function getCookies() {
+//     await getData(crawlCookies, optionsCookies).then(result => {
+//         debug('get Cookies OK !!!');
+//     });
+// }
+
