8b06b86f783d6decf5439b8f020d98e657a81e51
shi <shi@Shis-MacBook-Pro.local>
2019-08-23 23:50:43 +0700

    ok

--- options.js
+++ options.js
@@ -10,7 +10,7 @@ let optionsCookies = {
         accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'sec-fetch-user': '?1',
         'sec-fetch-mode': 'navigate',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
         'content-type': 'application/x-www-form-urlencoded',
         'upgrade-insecure-requests': '1',
         origin: 'https://plus.efxdata.com',
@@ -21,8 +21,8 @@ let optionsCookies = {
     {
         _username: 'huydn2010@gmail.com',
         _password: 'TraderViet13579',
-        swidth: '1077',
-        sheight: '927'
+        swidth: '1198',
+        sheight: '843'
     }
 }
 
@@ -31,14 +31,13 @@ let optionsBoards = {
     url: 'https://plus.efxdata.com/news/options_board',
     headers:
     {
-        'postman-token': '36ccb186-1e78-8ab0-ef05-5c05bbd70cb2',
         'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en;q=0.6,en-US;q=0.5',
-        referer: 'https://plus.efxdata.com/login',
+        referer: 'https://plus.efxdata.com/news/options_board',
         'sec-fetch-site': 'same-origin',
         accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'sec-fetch-user': '?1',
         'sec-fetch-mode': 'navigate',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
         dnt: '1',
         'upgrade-insecure-requests': '1',
         'cache-control': 'no-cache',
@@ -52,15 +51,14 @@ let optionsInsights = {
     url: 'https://plus.efxdata.com/news/insights',
     headers:
     {
-        'postman-token': 'fc1e4ba1-6902-6b6f-634c-6af200d80c45',
         'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en;q=0.6,en-US;q=0.5',
-        referer: 'https://plus.efxdata.com/news/events',
+        referer: 'https://plus.efxdata.com/news/insights',
         'sec-fetch-site': 'none',
         accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         dnt: '1',
         'sec-fetch-user': '?1',
         'sec-fetch-mode': 'navigate',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
         'upgrade-insecure-requests': '1',
         'cache-control': 'no-cache',
         pragma: 'no-cache',
@@ -73,15 +71,14 @@ let optionsDataPreviews = {
     url: 'https://plus.efxdata.com/news/events',
     headers:
     {
-        'postman-token': 'de38f1ac-93d6-e0ff-650c-cb0c7f0fdde4',
         'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en;q=0.6,en-US;q=0.5',
-        referer: 'https://plus.efxdata.com/news/insights',
+        referer: 'https://plus.efxdata.com/news/events',
         'sec-fetch-site': 'none',
         accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         dnt: '1',
         'sec-fetch-user': '?1',
         'sec-fetch-mode': 'navigate',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
         'upgrade-insecure-requests': '1',
         'cache-control': 'no-cache',
         pragma: 'no-cache',
--- schedule.js
+++ schedule.js
@@ -3,6 +3,8 @@ import { crawlInsights } from './helpers/parseinsights';
 import { crawlDataPreview } from './helpers/parsedatapreview';
 import { crawlCookies } from './helpers/fetchcookies';
 const moment = require('moment-timezone');
+const uuidv4 = require('uuid/v4');
+
 import { optionsCookies, optionsBoards, optionsDataPreviews, optionsInsights } from './options'
 
 let NewsModel = require('./models/news');
@@ -27,7 +29,6 @@ let filterDuplicateData = async (list) => {
 
 let _modules = [
     {
-        // schedule: "20 * * * *",
         fn: crawlInsights,
         option: optionsInsights,
         message: 'get Data crawlInsights OK !!!',
@@ -36,7 +37,6 @@ let _modules = [
         action: getData
     },
     {
-        // schedule: "40 * * * *",
         fn: crawlDataPreview,
         option: optionsDataPreviews,
         message: 'get Data crawlDataPreview OK !!!',
@@ -45,7 +45,6 @@ let _modules = [
         action: getData
     },
     {
-        // schedule: "0 * * * *",
         fn: crawlOptionBoards,
         option: optionsBoards,
         message: 'get Data crawlOptionBoards OK !!!',
@@ -55,59 +54,81 @@ let _modules = [
     }
 ]
 
+
+let rand = moment();
+
 let randomTime = () => {
-    let time = new Date();
+    let type = "minutes"
     let listtime = [];
-    // let min = 10;
-    // let max = 90;
-    // for (let i = 0; i < 3; i++) {
-    //     listtime.push(moment(time).add(parseInt(Math.random() * (+max - +min) + +min), 'seconds').toString());
-    // }
     let min = 10;
     let max = 30;
-    let rand = moment(time).add(parseInt(Math.random() * (+max - +min) + +min), 'seconds');
+    rand = moment(rand).add(parseInt(Math.random() * (+max - +min) + +min), type);
     listtime.push(rand.toString());
-    rand.add(parseInt(Math.random() * (+max - +min) + +min), 'seconds');
+    rand.add(parseInt(Math.random() * (+max - +min) + +min), type);
     listtime.push(rand.toString());
-    rand.add(parseInt(Math.random() * (+max - +min) + +min), 'seconds');
+    rand.add(parseInt(Math.random() * (+max - +min) + +min), type);
     listtime.push(rand.toString());
     return listtime;
 }
 
-
-let scheduleJobs = (listtime) => {
-    debug('Im in schedule', listtime)
+let scheduleJobs = (queue) => {
     for (const iterator of _modules) {
-        let time = listtime.shift();
+        let time = queue.schedule.shift();
         schedule.scheduleJob(time, async () => {
-            console.log("TIME", time, iterator);
-            // iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
-            //     .then(result => {
-            //         debug('after', time, ' ', iterator.message);
-            //     });
+            queue.count = queue.count + 1;
+            if (queue.count > 2) {
+                trace("first remove", queueList.length);
+                queueList = queueList.filter(q => {
+                    return queue.id !== q.id;
+                });
+                trace("last remove", queueList.length);
+            }
+            iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
+                .then(result => {
+                    debug('after', time, ' ', iterator.message);
+                });
         });
     }
 }
 
+let queueList = [];
+
+let i = 0;
 
 let boot = async () => {
-    debug("Start BOOT !!!");
+    debug("Start BOOT !!! ", queueList.length);
     let listtime = randomTime();
-    scheduleJobs(listtime);
-
-    // await getCookies();
-    // for (const iterator of _modules) {
-    //     iterator.model.create(await iterator.filter(await iterator.action(iterator.fn, iterator.option)))
-    //         .then(result => {
-    //             debug(iterator.message);
-    //         });
-    // }
+    if (queueList.length < 10) {
+        queueList.push({
+            id: uuidv4(),
+            schedule: listtime,
+            count: 0,
+            status: true,
+            added: false
+        });
+        trace("Push ", listtime.join(", "));
+        boot();
+    }
+    else {
+        i++;
+        trace("stop push queue");
+        setTimeout(boot, 30000);
+    }
 }
 
-boot()
+setInterval(() => {
+    for (const queue of queueList) {
+        queue.added = true;
+        scheduleJobs(queue);
+    }
+}, 10000);
 
-// async function getCookies() {
-//     await getData(crawlCookies, optionsCookies).then(result => {
-//         debug('get Cookies OK !!!');
-//     });
-// }
+async function getCookies() {
+    await getData(crawlCookies, optionsCookies).then(result => {
+        debug('get Cookies OK !!!');
+    });
+}
+
+setInterval(getCookies, 1000 * 60 * 60 * 11);
+getCookies();
+boot()
